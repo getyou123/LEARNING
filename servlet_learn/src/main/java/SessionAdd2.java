51e1756e9b285af4c9c19e2106af00d88497dc3f
haoguowang <1414645410@qq.com>
2023-01-28 15:34:11 +0800

    session中设置数据

---
+++ servlet_learn/src/main/java/SessionAdd2.java
@@ -0,0 +1,39 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/SessionAdd2")
+public class SessionAdd2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String id = session.getId();
+        long creationTime = session.getCreationTime();
+        long lastAccessedTime = session.getLastAccessedTime();
+        int count = 0;
+        try {
+            count = (int) session.getAttribute("count");
+        } catch (Exception e) {
+
+        }
+        resp.setContentType("text/html;charset=UTF-8");
+        if (count == 0) {
+            resp.getWriter().write(id + "首次session访问,session创建时间为" + creationTime + "，上次访问时间是" + lastAccessedTime);
+
+        } else {
+            resp.getWriter().write(id + "第" + count + "次访问，session创建时间为" + creationTime + "，上次访问时间是" + lastAccessedTime);
+        }
+        count += 2;
+        if (count > 20) count = 0;
+        session.setAttribute("count", count);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
