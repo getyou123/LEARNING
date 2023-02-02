755a12ff04de4e7a384a8e393c53bfeb12a4ad14
haoguowang <1414645410@qq.com>
2023-02-02 09:12:46 +0800

    ignore update

--- maven_learn/src/main/java/com/getyou123/imperial/court/servlet/module/AuthServlet.java
+++ maven_learn/src/main/java/com/getyou123/imperial/court/servlet/module/AuthServlet.java
@@ -34,8 +34,8 @@ public class AuthServlet extends ModelBaseServlet {
             session.setAttribute(ImperialCourtConst.LOGIN_EMP_ATTR_NAME, emp);
 
             // 5、前往指定页面视图
-            String templateName = "temp";
-            processTemplate(templateName, request, response);
+            response.sendRedirect(request.getContextPath() + "/work?method=showMemorialsDigestList");
+
 
         } catch (Exception e) {
             e.printStackTrace();
