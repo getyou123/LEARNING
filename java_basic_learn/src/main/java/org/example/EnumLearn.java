675b8fb045f27438681ae31b2eb06f628055c512
haoguowang <1414645410@qq.com>
2023-02-10 14:20:55 +0800

    update

---
+++ java_basic_learn/src/main/java/org/example/EnumLearn.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class EnumLearn {
+    enum Season {
+        SPRING("spring"),//此处调用了构造方法,默认public
+        SUMMER("summer");
+
+        private String season;//单例模式
+
+        private Season(String string) {
+            this.season = string;
+        }
+
+
+        public String getSeason() {
+            return season;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Season season = Season.SPRING;
+        System.out.println(season);
+
+    }
+
+}
