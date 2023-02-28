939f14d5e916817869ceaaa54ba1570e7650f229
haoguowang <1414645410@qq.com>
2023-02-28 17:33:34 +0800

    update

--- java_basic_learn/src/main/java/org/example/DateLearn.java
+++ java_basic_learn/src/main/java/org/example/DateLearn.java
@@ -2,6 +2,9 @@ package org.example;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -30,9 +33,21 @@ public class DateLearn {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        // date的两个构造函数
+        {
+            Date date = new Date();
+            System.out.println("当前时间戳：" + date.getTime());
+            System.out.println("当前时间：" + date);
 
-        //日期格式和字符串的转化
+            // 基于指定的时间戳
+            Date date1 = new Date(1677571316596L);
+            System.out.println("时间戳构造的时间：" + date1);
+
+            // toString 默认的格式 dow mon dd hh:mm:ss zzz yyyy
+        }
+
+        //SimpleDateFormat 实现日期格式和字符串的转化
         {
             //date->string
             Date date = new Date();//最新的时间
@@ -47,8 +62,20 @@ public class DateLearn {
                 e.printStackTrace();
             }
 
-        }
+            // 非单纯日期
+            Date d = new Date();
+            SimpleDateFormat sf = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒 SSS毫秒  E Z");
+            //把Date日期转成字符串，按照指定的格式转
+            System.out.print("特殊的日期格式：");
+            String str = sf.format(d);
+            System.out.println(str);
 
+            String str1 = "2022年06月06日 16时03分14秒 545毫秒  星期四 +0800";
+            Date d1 = sf.parse(str1);
+            System.out.print("特殊的日期格式：");
+            System.out.println(d1);
+
+        }
 
         //日期增加或者移动
         {
@@ -61,10 +88,97 @@ public class DateLearn {
         //获取时间戳
         {
             Long timeStamp = System.currentTimeMillis();//获取当前的时间戳
+            System.out.println("毫秒时间戳：" + timeStamp);// 毫秒时间戳
+
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             Date date = new Date(Long.parseLong(String.valueOf(timeStamp)));//时间戳转化成Date
+            System.out.println("date对象对应的时间戳：" + date.getTime());
             String sd = sdf.format(date);
             System.out.println(sd);
+        }
+
+        //Calendar作为新的获取日期的工具类
+        {
+            // 获取Calendar对象
+            Calendar calendar = Calendar.getInstance();
+            System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+            System.out.println(calendar.get(Calendar.DAY_OF_YEAR));
+
+            // 后移动
+            calendar.add(Calendar.DATE,1);
+            System.out.println(calendar.get(Calendar.DAY_OF_YEAR));
+
+            // 前移动
+            calendar.add(Calendar.DATE,-10);
+            System.out.println(calendar.get(Calendar.DAY_OF_YEAR));
+
+            // 转为date
+            System.out.println(calendar.getTime());
+
+            // 重置为某个date
+            calendar.setTime(new Date());
+            System.out.println(calendar.getTime());
+
+        }
+
+        //jdk8以及之后的api
+        {
+            System.out.println("jdk8以及之后的api:");
+            LocalDate localDate = LocalDate.now();
+            LocalTime localTime = LocalTime.now();
+            LocalDateTime localDateTime = LocalDateTime.now();
+
+            System.out.println(localDate);
+            System.out.println(localTime);
+            System.out.println(localDateTime);
+
+            // 获取指定的日期LocalDate
+            LocalDate localDate1 = LocalDate.of(2022, 12, 1);
+            System.out.println(localDate1);
+
+            // 获取指定的时间
+            LocalTime localTime1 = LocalTime.of(23, 13);
+            System.out.println(localTime1);
+
+            // 获取指定的datetime
+            LocalDateTime localDateTime1 = LocalDateTime.of(2022, 12, 31, 11, 30, 00);
+            System.out.println(localDateTime1);
+
+            // 日期前移，日期后移
+            System.out.println(localDate1.plusDays(2));
+            System.out.println(localDate1.minusDays(2));
+
+        }
+
+        {
+            System.out.println("jdk1.8之后的时间戳");
+            Instant instant = Instant.now();
+            System.out.println(instant);
+
+            // 时区转化
+            OffsetDateTime atOffset = instant.atOffset(ZoneOffset.ofHours(8));
+            System.out.println(atOffset);
+
+            // 时间戳->instant
+            Instant instant1 = Instant.ofEpochMilli(1677575957929L);
+            System.out.println(instant1);
+
+            // instant->时间戳
+            long l = atOffset.toInstant().toEpochMilli();
+            System.out.println(l);
+            System.out.println(instant.toEpochMilli());
+        }
+
+        // 字符串格式转化
+        {
+            System.out.println("字符串格式化");
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:MM:ss");
+            String string = dateTimeFormatter.format(LocalDateTime.now());
+            System.out.println(string);
+
+            LocalDate parsed = LocalDate.parse(string, dateTimeFormatter);
+            System.out.println(parsed);
+
         }
 
     }
--- java_basic_learn/src/main/java/org/example/StringLearn.java
+++ java_basic_learn/src/main/java/org/example/StringLearn.java
@@ -1,48 +1,128 @@
 package org.example;
 
+import org.junit.Test;
+import scala.Int;
+
+import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 import java.util.StringTokenizer;
 
 public class StringLearn {
+
+    // 关于字符串的存储位置
+    @Test
+    public void testLocation() {
+        String s1 = "for a test"; // 字面量，放在了字符串常量池，s1指向对象
+        String s2 = "for a test";
+        System.out.println(s1 == s2); // true 都是在常量池
+
+        String s3 = new String("hello");
+        String s4 = new String("hello");
+        System.out.println(s3 == s4); // false
+
+        s1 = "javaEE";
+        s2 = "javaEE";
+        s3 = new String("javaEE");
+        s4 = new String("javaEE");
+
+        System.out.println(s1 == s2);//true
+        System.out.println(s1 == s3);//false
+        System.out.println(s1 == s4);//false
+        System.out.println(s3 == s4);//false
+
+        // 常量+String对象的拼接是走的StringBuilder，然后调用toString方法
+        String s5 = "string " + new String("learn");
+        String s6 = "string learn";
+
+        System.out.println(s5 == s6); //false
+
+        // 常量+常量的 就是直接作为常量 final修饰也是一样的常量的
+        String s7 = "string " + "learn";
+        String s8 = "string learn";
+
+        System.out.println(s7 == s8); // true
+
+        // intern方法
+        // 说明：堆空间的对象在调用intern()之后，如果常量池中已有那么直接返回常量池的地址
+        // 如果没有的话，放入然后返回地址。
+        String s10 = s1.intern();
+        System.out.println(s1 == s10);// true
+
+
+    }
+
+    //创建字符串
+    @Test
+    public void testCreateString() {
+        String s1 = "for a test"; // 字面量，放在了字符串常量池，s1指向对象
+        String s2 = "for a test";
+        System.out.println(s1 == s2); // 都是在常量池
+        s2 = "test";
+        System.out.println(s1); // for a test s2使用新的常量，字符串不可变
+        String s = new String("test"); // 这就是在堆空间中申请了一块内存,前提是在常量池中有字面量，当然jdk7之后都是在堆中哈
+        System.out.println(s);
+    }
+
+
+    // 字符串连接 底层是new String
+    @Test
+    public void testConcatString() {
+        String s1 = "for a test";
+        String s = new String("test");
+        String concat = s1.concat(s);
+        System.out.println(concat);
+        System.out.println(s + s1);
+    }
+
+    // 转换&编码解码
+    @Test
+    public void testParse() throws UnsupportedEncodingException {
+
+        String s1 = "100";
+        // String -> Integer
+        int i = Integer.parseInt(s1);
+        System.out.println(i);
+
+        // string -> bytes
+        String s2 = "爱你中国";
+        byte[] bytes = s2.getBytes("gbk"); // 默认是utf-8
+        for (int j = 0; j < bytes.length; j++) {
+            System.out.print(bytes[j]);
+        }
+        System.out.println();
+
+        // bytes->String
+        String s = new String(bytes, "utf-8");// 编码和解码的字符集合需要相同
+        System.out.println(s);
+
+    }
+
+    // 删除某个位置的字符串
+    // 获取子串
+    @Test
+    public void testPos() {
+        //获取子串
+        String s1 = "for a test";
+        System.out.println("获取子串是不包含左闭右开的" + s1.substring(0, 2));// fo
+        String s = new String("test for pos");
+        int pos = 1;
+        System.out.println(s.substring(0, pos) + s.substring(pos + 1));
+    }
+
+
+    @Test
+    // 字符串查找
+    public void testIndexOf(){
+        // -1 表示没找到
+        String s1 = "for a test";
+        System.out.println(s1.indexOf('o'));
+        System.out.println(s1.lastIndexOf("a"));
+        System.out.println(s1.indexOf("a test", 2));//从指定的位置开始找
+        System.out.printf("一个串为 %s，一个数为 %d ", "hui", 34);
+    }
+
     public static void main(String[] args) {
 
-        //创建字符串
-        {
-            String s1 = "for a test";
-            System.out.println(s1);
-            String s = new String("jiushishuo");
-            System.out.println(s);
-        }
-
-        // 字符串连接
-        {
-            String s1 = "for a test";
-            String s = new String("jiushishuo");
-            String concat = s1.concat(s);
-            System.out.println(concat);
-            System.out.println(s + s1);
-        }
-
-        // 删除某个位置的字符串
-        // 获取子串
-        {
-            //获取子串
-            String s1 = "for a test";
-            System.out.println("获取子串是不包含左闭右开的" + s1.substring(1, 2));
-            String s = new String("jiushishuo");
-            int pos = 1;
-            System.out.println(s.substring(0, pos) + s.substring(pos + 1));
-        }
-
-        // 字符串查找
-        {
-            // -1 表示没找到
-            String s1 = "for a test";
-            System.out.println(s1.indexOf('o'));
-            System.out.println(s1.lastIndexOf("a"));
-            System.out.println(s1.indexOf("a test", 2));//从指定的位置开始找
-            System.out.printf("一个串为 %s，一个数为 %d ", "hui", 34);
-        }
 
         //长度
         {
